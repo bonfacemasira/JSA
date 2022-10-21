fd579ccd8a7962156ae49045a5f3e11d1be67ef4
Bonface Masira <bonfacemasira@gmail.com>
2022-10-21 14:07:38 +0300

    create NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,9 +1,17 @@
+import "../App.css";
 import React from "react";
+import { Link } from "react-router-dom";
 
 function NavBar() {
 
   return (
-    <div></div>
+    <div className="wrapper">
+      <h1 className="logo">JS</h1>
+      <nav className="Nav">
+        <button className="registerButton" as={Link} to="/login" >Login</button>
+        <button className="registerButton" as={Link} to="/register">Sign Up</button>
+      </nav>
+    </div>
   );
 }
 
