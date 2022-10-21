1d5d2653d453fbf2c4a779265a4d81a0bf15de4d
Bonface Masira <bonfacemasira@gmail.com>
2022-10-21 13:35:46 +0300

    implement auto-login and import NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
 import "./App.css";
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
+import NavBar from "./components/NavBar";
 
 // import LoginForm from "./components/LoginForm";
 // import RegisterForm from "./components/RegisterForm";
 
-
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
   return (
-    <div className="App">
-      {/* <LoginForm /> */}
-    </div>
-  );
+  <div className="App">
+    <NavBar />
+  </div>);
 }
 
 export default App;
